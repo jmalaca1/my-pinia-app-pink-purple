c1453e1c5ece8b75e1da711530a1ec3403668b5d
Jacqueline MalacaraOrtega <jmalaca1@stu.southtexascollege.edu>
2026-05-11 00:12:48 -0500

    fixing errors p.2

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    cloudflare(),
     vueDevTools(),
   ],
   resolve: {
